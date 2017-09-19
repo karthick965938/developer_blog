1da2526539397c2dd7548563611a3b60efce0dc1
vasanthakumar <vasanthakumar@aryvartdev.com>
2017-09-19 11:00:30 +0530

    js issue solved

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,13 @@
 //= require turbolinks
 //= require_tree .
 document.addEventListener("turbolinks:load", function() {
+	$("#addClass").click(function () {
+		$('#sidebar_secondary').addClass('popup-box-on');
+	});
+
+	$("#removeClass").click(function () {
+		$('#sidebar_secondary').removeClass('popup-box-on');
+	});
 	// Smooth scrolling using jQuery easing
 	$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
 		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
