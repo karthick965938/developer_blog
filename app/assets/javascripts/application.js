bbd4729ad810d28c8b17290594b3c5a5f39cf87a
vasanthakumar <vasanthakumar@aryvartdev.com>
2017-09-26 12:33:18 +0530

    partial

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,39 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+document.addEventListener("turbolinks:load", function() {
+	$("#addClass").click(function () {
+		$('#sidebar_secondary').addClass('popup-box-on');
+	});
+
+	$("#removeClass").click(function () {
+		$('#sidebar_secondary').removeClass('popup-box-on');
+	});
+	// Smooth scrolling using jQuery easing
+	$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html, body').animate({
+					scrollTop: (target.offset().top - 48)
+				}, 1000, "easeInOutExpo");
+				return false;
+			}
+		}
+	});
+
+	// Closes responsive menu when a scroll trigger link is clicked
+	$('.js-scroll-trigger').click(function() {
+		$('.navbar-collapse').collapse('hide');
+	});
+
+});
+
+$(window).scroll(function() {
+	if ($("#mainNav").offset().top > 100) {
+		$("#mainNav").addClass("navbar-shrink");
+	} else {
+		$("#mainNav").removeClass("navbar-shrink");
+	}
+});
